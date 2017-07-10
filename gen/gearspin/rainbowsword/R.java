c8d2e44bfbcbc3e3292a0f171d3ec465dd90d3d3
EnderGeneral149 <herobrinebudder149@gmail.com>
2017-07-09 23:31:35 -0500

    just a test

--- gen/gearspin/rainbowsword/R.java
+++ gen/gearspin/rainbowsword/R.java
@@ -10,7 +10,12 @@ package gearspin.rainbowsword;
 public final class R {
     public static final class attr {
     }
+    public static final class layout {
+        public static final int main=0x7f020000;
+    }
     public static final class string {
-        public static final int app_name=0x7f020000;
+        public static final int app_name=0x7f030000;
+        public static final int twitter=0x7f030001;
+        public static final int yt=0x7f030002;
     }
 }
